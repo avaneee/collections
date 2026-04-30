2efa80e9303b25ebef402286050126fbecf74f70
avaneee <avaneedalmia19@gmail.com>
2026-04-30 03:46:19 -0400

    minor homepage edits, starting
    3:45am added some js to homepage

--- homepage.js
+++ homepage.js
@@ -1,1 +1,13 @@
 // for this i want to have the main links just moving around and stuff probably? some sort of randomising (one postcard, one scrap, one magnet that people can click on)
+
+
+// from small sites homepage
+
+function rotateLinks() {
+	let allSpans = document.querySelectorAll('span');
+	allSpans.forEach((element) => {
+		let randomDeg = Math.random() * 20;
+		element.style.transform = `rotate(${randomDeg}deg)`;
+	});
+}
+setInterval(rotateLinks, 500);
